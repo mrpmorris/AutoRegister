7c5d6b71afc1b21a9d708b5bce987d7daee30ec1
Peter Morris <mrpmorris@gmail.com>
2025-01-03 10:39:56 +0000

    No weavers found for the configuration entries Morris.AutoInject

---
+++ Source/Lib/Morris.AutoInjectTests/ModuleWeaverTests/Test1.cs
@@ -0,0 +1,21 @@
+﻿using Morris.AutoInject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morris.AutoInjectTests.ModuleWeaverTests;
+
+[TestClass]
+public class Test1
+{
+	[TestMethod]
+	public void X() { }
+}
+
+[AutoInject(Find.DescendantsOf, typeof(System.Object), RegisterAs.FirstDiscoveredInterface, WithLifetime.Scoped)]
+public partial class MyModule
+{
+
+}
