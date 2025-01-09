8297e3bcc4fd2a47e62fae53dd1a2f3d5c0831ba
Peter Morris <mrpmorris@gmail.com>
2025-01-09 13:30:24 +0000

    WhenRegisteringAsBaseType_ThenTypeFromFindCriteriaIsUsedAsServiceType

--- Source/Lib/Morris.AutoInjectTests/ModuleWeaverTests/RegisterAsTests.cs
+++ Source/Lib/Morris.AutoInjectTests/ModuleWeaverTests/RegisterAsTests.cs
@@ -61,4 +61,59 @@ public class RegisterAsTests
 				)
 			]);
 	}
+
+	[TestMethod]
+	public void WhenRegisteringAsBaseType_ThenTypeFromFindCriteriaIsUsedAsServiceType()
+	{
+		string sourceCode =
+			"""
+			using Morris.AutoInject;
+
+			namespace MyNamespace;
+			[AutoInject(Find.AnyTypeOf, typeof(IMarker), RegisterAs.BaseType, WithLifetime.Scoped)]
+			public partial class MyModule
+			{
+			}
+
+			public interface IMarker {}
+			public class QualifyingClass1 : IMarker {}
+			public class QualifyingClass2 : IMarker {}
+			public class QualifyingClass3 : QualifyingClass1 {}
+			""";
+
+		WeaverExecutor.Execute(sourceCode, out Fody.TestResult? fodyTestResult, out string? manifest);
+
+		RegistrationHelper.AssertRegistration(
+			assembly: fodyTestResult.Assembly,
+			manifest: manifest,
+			expectedModuleRegistrations:
+			[
+				new(
+					classFullName: "MyNamespace.MyModule",
+					autoInjectAttributes:
+					[
+						new(
+							find: Find.AnyTypeOf,
+							typeFullName: "MyNamespace.IMarker",
+							registerAs: RegisterAs.BaseType,
+							withLifetime: WithLifetime.Scoped)
+					],
+					services:
+					[
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.IMarker",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass1"),
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.IMarker",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass2"),
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.IMarker",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass3"),
+					]
+				)
+			]);
+	}
 }
