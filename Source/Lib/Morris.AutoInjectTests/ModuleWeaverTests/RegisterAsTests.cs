82bd06aee1d6ba9cc1d75384c815bebefb534c82
Peter Morris <mrpmorris@gmail.com>
2025-01-09 13:27:18 +0000

    WhenRegisteringAsDiscoveredClass_ThenDiscoveredClassIsUsedAsServiceType

---
+++ Source/Lib/Morris.AutoInjectTests/ModuleWeaverTests/RegisterAsTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Morris.AutoInject;
+using Morris.AutoInjectTests.Helpers;
+
+namespace Morris.AutoInjectTests.ModuleWeaverTests;
+
+[TestClass]
+public class RegisterAsTests
+{
+	[TestMethod]
+	public void WhenRegisteringAsDiscoveredClass_ThenDiscoveredClassIsUsedAsServiceType()
+	{
+		string sourceCode =
+			"""
+			using Morris.AutoInject;
+
+			namespace MyNamespace;
+			[AutoInject(Find.AnyTypeOf, typeof(IMarker), RegisterAs.DiscoveredClass, WithLifetime.Scoped)]
+			public partial class MyModule
+			{
+			}
+
+			public interface IMarker {}
+			public class QualifyingClass1 : IMarker {}
+			public class QualifyingClass2 : IMarker {}
+			public class QualifyingClass3 : QualifyingClass1 {}
+			""";
+
+		WeaverExecutor.Execute(sourceCode, out Fody.TestResult? fodyTestResult, out string? manifest);
+
+		RegistrationHelper.AssertRegistration(
+			assembly: fodyTestResult.Assembly,
+			manifest: manifest,
+			expectedModuleRegistrations:
+			[
+				new(
+					classFullName: "MyNamespace.MyModule",
+					autoInjectAttributes:
+					[
+						new(
+							find: Find.AnyTypeOf,
+							typeFullName: "MyNamespace.IMarker",
+							registerAs: RegisterAs.DiscoveredClass,
+							withLifetime: WithLifetime.Scoped)
+					],
+					services:
+					[
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.QualifyingClass1",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass1"),
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.QualifyingClass2",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass2"),
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.QualifyingClass3",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass3"),
+					]
+				)
+			]);
+	}
+}
