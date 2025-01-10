b37bf9f43fece1327cccba15b988110fba136991
Peter Morris <mrpmorris@gmail.com>
2025-01-10 11:46:30 +0000

    Test for registering base closed generic on an interface

--- Source/Lib/Morris.AutoInjectTests/ModuleWeaverTests/RegisterAsTests.cs
+++ Source/Lib/Morris.AutoInjectTests/ModuleWeaverTests/RegisterAsTests.cs
@@ -118,7 +118,7 @@ public class RegisterAsTests
 	}
 
 	[TestMethod]
-	public void WhenRegisteringAsBaseClosedGenericType_ThenClosedGenericTypeFromFindCriteriaIsUsedAsServiceType()
+	public void WhenRegisteringAsBaseClosedGenericType_AndFindCriteriaIsAClass_ThenClosedGenericTypeFromFindCriteriaIsUsedAsServiceType()
 	{
 		string sourceCode =
 			"""
@@ -173,6 +173,68 @@ public class RegisterAsTests
 						new(
 							lifetime: ServiceLifetime.Scoped,
 							serviceTypeFullName: "MyNamespace.BaseClass<System.Int32, System.String>",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass4"),
+					]
+				)
+			]);
+	}
+
+	[TestMethod]
+	public void WhenRegisteringAsBaseClosedGenericType_AndFindCriteriaIsAnInterface_ThenClosedGenericTypeFromFindCriteriaIsUsedAsServiceType()
+	{
+		string sourceCode =
+			"""
+			using Morris.AutoInject;
+
+			namespace MyNamespace;
+			[AutoInject(Find.DescendantsOf, typeof(IBaseInterface<,>), RegisterAs.BaseClosedGenericType, WithLifetime.Scoped)]
+			public partial class MyModule
+			{
+			}
+
+			public interface IBaseInterface<TKey, TValue> {}
+			public interface IIntBasedValue<T> : IBaseInterface<int, T> {}
+
+			public class QualifyingClass1 : IBaseInterface<int, string> {}
+			public class QualifyingClass2 : IBaseInterface<int, string> {}
+			public class QualifyingClass3 : IIntBasedValue<string> {}
+			public class QualifyingClass4 : QualifyingClass3 {}
+			""";
+
+		WeaverExecutor.Execute(sourceCode, out Fody.TestResult? fodyTestResult, out string? manifest);
+
+		RegistrationHelper.AssertRegistration(
+			assembly: fodyTestResult.Assembly,
+			manifest: manifest,
+			expectedModuleRegistrations:
+			[
+				new(
+					classFullName: "MyNamespace.MyModule",
+					autoInjectAttributes:
+					[
+						new(
+							find: Find.DescendantsOf,
+							typeFullName: "MyNamespace.IBaseInterface`2",
+							registerAs: RegisterAs.BaseClosedGenericType,
+							withLifetime: WithLifetime.Scoped)
+					],
+					services:
+					[
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.IBaseInterface<System.Int32, System.String>",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass1"),
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.IBaseInterface<System.Int32, System.String>",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass2"),
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.IBaseInterface<System.Int32, System.String>",
+							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass3"),
+						new(
+							lifetime: ServiceLifetime.Scoped,
+							serviceTypeFullName: "MyNamespace.IBaseInterface<System.Int32, System.String>",
 							serviceImplementationTypeFullName: "MyNamespace.QualifyingClass4"),
 					]
 				)
