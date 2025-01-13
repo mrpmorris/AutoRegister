579370b67cb32b7cad43a202f95735f9e76051de
Peter Morris <mrpmorris@gmail.com>
2025-01-13 11:14:13 +0000

    Single silly test

---
+++ Source/Lib/Morris.AutoRegisterTests/SillyTest.cs
@@ -0,0 +1,13 @@
+﻿using Morris.AutoRegister.Fody;
+
+namespace Morris.AutoRegisterTests;
+
+[TestClass]
+public class SillyTest
+{
+	[TestMethod]
+	public void Test()
+	{
+		Assert.AreEqual("Morris.AutoRegister.Fody.ModuleWeaver", typeof(ModuleWeaver).FullName);
+	}
+}
