d4a1ea5aa4ccea7e2d58830f7bdc2c2e4f3dd170
Peter Morris <mrpmorris@gmail.com>
2025-01-13 11:22:48 +0000

    Don't descend ModuleWeaver from BaseModuleWeaver

--- Source/Lib/Morris.AutoRegister.Fody/ModuleWeaver.cs
+++ Source/Lib/Morris.AutoRegister.Fody/ModuleWeaver.cs
@@ -1,25 +1,5 @@
-﻿using Fody;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿namespace Morris.AutoRegister.Fody;
 
-namespace Morris.AutoRegister.Fody;
-
-public class ModuleWeaver : BaseModuleWeaver
+public class ModuleWeaver
 {
-	public override IEnumerable<string> GetAssembliesForScanning()
-	{
-		yield return "netstandard";
-		yield return "mscorlib";
-		yield return "Microsoft.Extensions.DependencyInjection.Abstractions";
-	}
-
-	public override void Execute()
-	{
-	}
-
 }
