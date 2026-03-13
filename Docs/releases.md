7ae5bd4d1ad7a05f67bff591519de54cce777d3c
Peter Morris <mrpmorris@gmail.com>
2026-03-13 18:16:50 +0000

    Allow service keys to be defined in another assembly - Fixes #4

--- Docs/releases.md
+++ Docs/releases.md
@@ -1,4 +1,7 @@
-FirsFirstInt# Releases
+# Releases
+
+## Version 1.1
+- Allow service keys to be declared in a different assembly ([#4](https://github.com/mrpmorris/AutoRegister/issues/4))
 
 ## Version 1.0
 First production-ready release.
---
+++ Source/Lib/Morris.AutoRegister.Fody/Extensions/TypeReferenceDescendsFromExtension.cs
@@ -0,0 +1,31 @@
+﻿using Mono.Cecil;
+using System.Linq;
+
+namespace Morris.AutoRegister.Fody.Extensions;
+
+internal static class TypeReferenceDescendsFromExtension
+{
+	public static bool DescendsFrom(this TypeReference child, TypeReference baseType)
+	{
+		TypeDefinition childDefinition = child.Resolve();
+
+		if (childDefinition.IsClass)
+		{
+			TypeReference? current = childDefinition.BaseType;
+			while (current is not null)
+			{
+				if (current.IsSameAs(baseType))
+					return true;
+
+				current = current.Resolve().BaseType;
+			}
+		}
+		else if (childDefinition.IsInterface)
+		{
+			if (!child.IsSameAs(baseType) && childDefinition.Interfaces.Any(x => x.InterfaceType.IsSameAs(baseType)))
+				return true;
+		}
+
+		return false;
+	}
+}
